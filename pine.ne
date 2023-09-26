3baf0e62af45ff4fe8902888f5ccf6a8dd28e492
Justin Palmer <encytemedia@gmail.com>
2023-09-26 14:39:56 -0500

    add some wholes and fractions

--- pine.ne
+++ pine.ne
@@ -16,6 +16,12 @@ main -> measurement
 
 measurement -> foot | inch
 foot -> int "'"
-inch -> int %ws fraction %primeinch | int %ws fraction | fraction %primeinch | fraction | int "\"" | int
+inch -> int %primefoot %ws int %ws fraction %primeinch   
+	| int %primefoot %ws int %primeinch  
+	| int %primefoot %ws fraction %primeinch 
+	| int %ws fraction %primefoot 
+	| int %ws fraction %primeinch 
+	| int %ws fraction 
+	| fraction %primeinch | fraction | int "\"" | int
 fraction -> int "/" int
 int -> %number:*
--- tests/parse.test.js
+++ tests/parse.test.js
@@ -10,10 +10,30 @@ test("explicit foot", () => {
   expect(parse(`2'`)).not.toThrowError();
 });
 
-test("implicit inch fraction", () => {
+test("implicit fraction inch", () => {
   expect(parse(`1/2`)).not.toThrowError();
 });
 
-test("explicit inch fraction", () => {
+test("explicit fraction inch", () => {
   expect(parse(`1/2"`)).not.toThrowError();
+});
+
+test("implicit whole and fraction inch", () => {
+  expect(parse(`3 1/2`)).not.toThrowError();
+});
+
+test("explicit whole and fraction inch", () => {
+  expect(parse(`3 1/2"`)).not.toThrowError();
+});
+
+test("explicit whole and fraction foot", () => {
+  expect(parse(`3 1/2'`)).not.toThrowError();
+});
+
+test("explicit whole foot and whole inch", () => {
+  expect(parse(`3' 5"`)).not.toThrowError();
+});
+
+test("explicit whole foot and fraction inch", () => {
+  expect(parse(`3' 1/2"`)).not.toThrowError();
 });
