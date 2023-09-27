0f7d5071c482840b4d009e4817996aaf8d89a46a
Justin Palmer <encytemedia@gmail.com>
2023-09-27 11:47:54 -0500

    more arithmetic tests

--- tests/arithmetic.test.js
+++ tests/arithmetic.test.js
@@ -1,5 +1,23 @@
-test("implicit inch", () => {
+test("implicit inch addition", () => {
   expect(() => {
     expect(parse(`1 + 1`)).toBe(2);
+  }).not.toThrowError();
+});
+
+test("explicit inch addition", () => {
+  expect(() => {
+    expect(parse(`1" + 21"`)).toBe(22);
+  }).not.toThrowError();
+});
+
+test("implicit inch plus explicit foot", () => {
+  expect(() => {
+    expect(parse(`1 + 2'`)).toBe(25);
+  }).not.toThrowError();
+});
+
+test("explicit foot addition", () => {
+  expect(() => {
+    expect(parse(`1' + 2'`)).toBe(36);
   }).not.toThrowError();
 });
