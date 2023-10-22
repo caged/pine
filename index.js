682d01417381aefc06724b8ec8ebf5db834e351d
Justin Palmer <encytemedia@gmail.com>
2023-10-22 13:05:59 -0500

    slightly better name for mixed fractions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const grammar = require("./pine");
-const { gcd, fraction, convert } = require("./src/lib");
+const { gcd, fraction, mixedFraction } = require("./src/lib");
 
-module.exports = { grammar, gcd, fraction, convert };
+module.exports = { grammar, gcd, fraction, mixedFraction };
--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,28 @@
 const NEAREST = 16.0;
 
+// Given two numbers, find the greatest common denominator
+//
+// Example:
+//   gcd(12, 16) => 4
+//   gcd(0, 1) => 1
+//
+// Returns: number
 function gcd(a, b) {
   if (b < 1 / NEAREST) return a;
 
   return gcd(b, Math.floor(a % b));
 }
 
+// Given a decimal, convert to the nearest 1/16th fraction.
+//
+// Example:
+//   fraction(0.125) => "1/8"
+//   fraction(0.25) => "1/4"
+//   fraction(2 / 16.0) => "1/8"
+//   fraction(0.00001) => "1/16"
+//   fraction(1.5) => "3/2"
+//
+// Returns: string
 function fraction(decimal) {
   const number = (Math.round(decimal * NEAREST) / NEAREST).toFixed(NEAREST / 2);
   const len = number.toString().length - 2;
@@ -22,9 +39,17 @@ function fraction(decimal) {
   )}`;
 }
 
-function convert(number) {
-  if (isNaN(number)) return null;
-  if (Number.isInteger(number)) return number;
+// Given a float or integer, convert to a whole and fraction
+//
+// Example:
+//   mixedFraction(1) => 1
+//   mixedFraction(0.125) => "1/8"
+//   mixedFraction(1.25) => "1 1/4"
+//
+// Returns: string
+function mixedFraction(number) {
+  if (isNaN(number)) throw new Error("Invalid input. Must be a number.");
+  if (Number.isInteger(number)) return String(number);
 
   const whole = Math.floor(number);
   const decimal = number % 1;
@@ -32,4 +57,4 @@ function convert(number) {
   return `${whole === 0 ? "" : whole + " "}${fraction(decimal)}`;
 }
 
-module.exports = { gcd, fraction, convert };
+module.exports = { gcd, fraction, mixedFraction };
